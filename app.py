6118126be9c6515b862e1b92a75c8a642c637865
tiffanyng23 <ngtiffany23@gmail.com>
2024-01-08 18:50:12 -0500

    updates

--- app.py
+++ app.py
@@ -295,4 +295,5 @@ def update_scatter(scatter_var_1, scatter_var_2):
 
 #run app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
